a1f8bc285536619328d9d5b482d17929236640e4
Denver-Meadows <meadows8200@gmail.com>
2022-03-02 10:22:36 -0500

    basic navbar setup complete

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,30 @@
 import React from "react";
 import "../styles/navbar.scss";
+import { BiUserCircle, BiSearch } from "react-icons/bi";
 
 const navbar = () => {
   return (
     <nav className="nav">
-      <div className="nav__logo">
-        <p>Denver Meadows</p>
+      <div className="nav__top">
+        <div className="nav__top__logo">
+          <p>Denver Meadows</p>
+        </div>
+        <div className="nav__top__search-login">
+          <p>
+            <BiUserCircle />
+          </p>
+          <p>
+            <BiSearch />
+          </p>
+        </div>
       </div>
-      <ul className="nav__list">
-        <li className="nav__list-link">Coding</li>
-        <li className="nav__list-link">Reading</li>
-        <li className="nav__list-link">Runnning</li>
-        <li className="nav__list-link">About</li>
-      </ul>
-      <div className="nav__search-login">
-        <p>login</p>
-        <p>search</p>
+      <div className="nav__bottom">
+        <ul className="nav__bottom__list">
+          <li className="nav__bottom__list-link">Coding</li>
+          <li className="nav__bottom__list-link">Reading</li>
+          <li className="nav__bottom__list-link">Runnning</li>
+          <li className="nav__bottom__list-link">About</li>
+        </ul>
       </div>
     </nav>
   );
