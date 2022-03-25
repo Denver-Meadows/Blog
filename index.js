e65e0b750dd97a6900690f79d5beb3aa025e73e4
Denver-Meadows <meadows8200@gmail.com>
2022-03-25 11:07:59 -0400

    server connected to client

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Home Page");
